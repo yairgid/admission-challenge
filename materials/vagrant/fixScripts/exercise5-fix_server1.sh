61df6ec73448a4938753d8fc4c37cbcb2ca9c8b0
yairgid <126573986+yairgid@users.noreply.github.com>
2023-08-31 17:01:04 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+sudo apt-get install sshpass -y
 
-#ssh vagrant@server2
-ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y
-ssh-copy-id -i ~/.ssh/id_rsa.pub server2
-sudo service ssh restart
+ssh-keygen -t rsa -N '' -f /home/vagrant/.ssh/id_rsa <<< y
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
+
+chmod 764 /home/vagrant/.ssh/id_rsa.pub
+echo 'sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa.pub vagrant@192.168.60.11' > /home/vagrant/sshing.sh
+
+chown vagrant:vagrant /home/vagrant/sshing.sh
+chmod 774 /home/vagrant/sshing.sh
+
+sudo sed -i '$a\    StrictHostKeyChecking no' /etc/ssh/ssh_config
